dcf0ebdc8e98231a78cbd7714bb582bfe9880a37
Felix Angelov <felangelov@gmail.com>
2021-03-01 23:13:12 -0600

    refactor

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,9 +1,9 @@
-import 'package:bloc_sample/src/authentication.dart';
 import 'package:bloc_sample/src/home_bloc.dart';
 import 'package:bloc_sample/src/home_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:bloc_sample/src/authentication_bloc.dart';
 
 @immutable
 class Home extends StatelessWidget {
@@ -11,78 +11,81 @@ class Home extends StatelessWidget {
     Key key,
   }) : super(key: key);
   @override
-  Widget build(BuildContext context) => RepositoryProvider<HomeRepository>(
-        create: (context) => HomeRepositoryImpl(
-          user: Authentication.of(context).user,
-          sharedPreferences: RepositoryProvider.of<SharedPreferences>(
-            context,
-            listen: true,
+  Widget build(BuildContext context) {
+    final user = context.select(
+      (AuthenticationBLoC bloc) => bloc.state
+          .maybeWhen(authenticated: (user) => user, orElse: () => null),
+    );
+    return RepositoryProvider<HomeRepository>(
+      create: (context) => HomeRepositoryImpl(
+        user: user,
+        sharedPreferences: context.read<SharedPreferences>(),
+      ),
+      child: BlocProvider<HomeBLoC>(
+        create: (context) => HomeBLoC(
+          repository: context.read<HomeRepository>(),
+        )..add(
+            const HomeEvent.fetch(),
           ),
-        ),
-        child: BlocProvider<HomeBLoC>(
-          create: (context) => HomeBLoC(
-            repository: RepositoryProvider.of<HomeRepository>(
-              context,
-              listen: true,
+        child: Builder(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              title: const Text('Home'),
+              actions: [
+                IconButton(
+                  onPressed: () =>
+                      context.read<AuthenticationBLoC>().add(const LogOut()),
+                  icon: const Icon(Icons.logout),
+                ),
+              ],
             ),
-          )..add(
-              const HomeEvent.fetch(),
+            floatingActionButton: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  iconSize: 48,
+                  splashRadius: 24,
+                  color: Colors.green,
+                  icon: const Icon(Icons.add_circle_outline),
+                  onPressed: () => BlocProvider.of<HomeBLoC>(context)
+                      .add(const HomeEvent.increment()),
+                ),
+                IconButton(
+                  iconSize: 48,
+                  splashRadius: 24,
+                  color: Colors.red,
+                  icon: const Icon(Icons.remove_circle_outline),
+                  onPressed: () => BlocProvider.of<HomeBLoC>(context)
+                      .add(const HomeEvent.decrement()),
+                ),
+              ],
             ),
-          child: Builder(
-            builder: (context) => Scaffold(
-              appBar: AppBar(
-                title: const Text('Home'),
-                actions: [
-                  IconButton(
-                    onPressed: () => Authentication.of(context).logout(),
-                    icon: const Icon(Icons.logout),
-                  ),
-                ],
-              ),
-              floatingActionButton: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    iconSize: 48,
-                    splashRadius: 24,
-                    color: Colors.green,
-                    icon: const Icon(Icons.add_circle_outline),
-                    onPressed: () => BlocProvider.of<HomeBLoC>(context).add(const HomeEvent.increment()),
-                  ),
-                  IconButton(
-                    iconSize: 48,
-                    splashRadius: 24,
-                    color: Colors.red,
-                    icon: const Icon(Icons.remove_circle_outline),
-                    onPressed: () => BlocProvider.of<HomeBLoC>(context).add(const HomeEvent.decrement()),
-                  ),
-                ],
-              ),
-              body: SafeArea(
-                child: DefaultTextStyle(
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text('USER #${Authentication.of(context).user.id}'),
-                      BlocBuilder<HomeBLoC, HomeState>(
-                        builder: (context, state) => state.map<Widget>(
-                          fetching: (_) => const Text('LOADING...'),
-                          received: (state) => Text('DATA: ${state.data}'),
-                        ),
-                      )
-                    ],
-                  ),
+            body: SafeArea(
+              child: DefaultTextStyle(
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text('USER #${user.id}'),
+                    BlocBuilder<HomeBLoC, HomeState>(
+                      builder: (context, state) => state.map<Widget>(
+                        fetching: (_) => const Text('LOADING...'),
+                        received: (state) => Text('DATA: ${state.data}'),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
           ),
         ),
-      );
+      ),
+    );
+  }
 }
